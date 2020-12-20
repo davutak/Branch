a94354ba30cea2e69d1e360f4126223c2b322f60
Davut Akhoroz <dakhoroz@gmail.com>
2020-12-20 23:53:16 +0100

    2-Sepete Ekle Kismi Yapildi

--- src/test1.java
+++ src/test1.java
@@ -1,5 +1,7 @@
 public class test1 {
     public static void main(String[] args) {
         System.out.println("Ürünleri Listele Kismi Yapildi");
+        System.out.println("Sepete Ekle Kismi Yapildi Kismi Yapildi");
+
     }
 }

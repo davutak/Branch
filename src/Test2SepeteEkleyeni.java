c4e4bc711fea755c0512b8202a2b6f2783315981
Davut Akhoroz <dakhoroz@gmail.com>
2020-12-21 00:08:38 +0100

    4-Sepete Ekle

--- src/Test2SepeteEkleyeni.java
+++ src/Test2SepeteEkleyeni.java
@@ -1,5 +1,5 @@
 public class Test2SepeteEkleyeni {
     public static void main(String[] args) {
-        System.out.println("Yeni Sepete ekle odulü");
+        System.out.println("Yeni Sepete ekle odulüm");
     }
 }
